fbcbd4b5bb208c1035646b6a068a3346e0be0fbd
Ryan Frazier <pianomanfrazier@gmail.com>
2016-11-21 13:01:11 -0700

    add id, title to notes

--- public/notes.js
+++ public/notes.js
@@ -1,7 +1,10 @@
+//this global will be replaced with the id from mongo
+//each entry gets assigned its own _id
+var id = 0;
+
 $( document ).ready( function() {
   var table = $("#noteTable");
   var add = $("#create-note");
-
   table.on("click",".del-note", deleteNote );
   table.on("click",".edit-note", editNote );
   table.on("click",".submit-edit", submitEdit );
@@ -17,26 +20,31 @@ function deleteNote() {
 
 function editNote() {
   var note = $(this).parent().siblings(".note").text();
+  var title = $(this).parent().siblings(".title").text();
+  var id = $(this).parent().siblings(".id").text();
   var row = $(this).closest("tr");
-  console.log(note);
-  row.replaceWith("<tr><td class=\"note\"><textarea class=\"form-control\">" + note + "</textarea></td><td><button class=\"submit-edit btn btn-success btn-sm\" type=\"button\">Submit Edit</button></td><td></td></tr>");
+  row.replaceWith("<tr><td class=\"id\">" + id + "</td><td class=\"title\"><textarea class=\"form-control\">" + title + "</textarea></td><td class=\"note\"><textarea class=\"form-control\">" + note + "</textarea></td><td><button class=\"submit-edit btn btn-success btn-sm\" type=\"button\">Submit Edit</button></td><td></td></tr>");
 }
 
 function submitEdit() {
   var note = $(this).parent().siblings(".note").children("textarea").val();
+  var title = $(this).parent().siblings(".title").children("textarea").val();
+  var id = $(this).parent().siblings(".id").text();
   var row = $(this).closest("tr");
   console.log(note);
-  row.replaceWith(newRow(note));
+  row.replaceWith(newRow(id, title,  note));
 }
 
 function addNote() {
   var noteArea = $("#note-text");
+  var noteTitle = $("#note-title");
   var note = noteArea.val();
-  console.log(note);
-  $("#noteTable tr:last").after(newRow(note)); 
-  noteArea.val('');
+  var title = noteTitle.val();
+  $("tbody").append(newRow(id++, title, note));
+  noteArea.val(''); //clear the textarea
+  noteTitle.val(''); //clear the textarea
 }
 
-function newRow(note) {
-  return "<tr><td class=\"note\"><p>" + note + "</p></td><td><button class=\"del-note btn btn-warning btn-sm\" type=\"button\">Delete</button></td><td><button class=\"edit-note btn btn-info btn-sm\" type=\"button\">Edit</button></td></tr>";
+function newRow(id, title, note) {
+  return "<tr><td class=\"id\">" + id + "</td><td class=\"title\">" + title + "</td><td class=\"note\"><p>" + note + "</p></td><td><button class=\"del-note btn btn-warning btn-sm\" type=\"button\">Delete</button></td><td><button class=\"edit-note btn btn-info btn-sm\" type=\"button\">Edit</button></td></tr>";
 }
